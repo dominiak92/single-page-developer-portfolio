b27b90e52ee6f75e763eb384600802049d80ca54
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-01-29 20:45:52 +0100

    styling for mobile, swiper, and contact form

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-// let swiper = new Swiper(".slide-content", {
-//     slidesPerView: 3,
-//     spaceBetween: 25,
-//     loop: true,
-//     centerSlide: 'true',
-//     fade: 'true',
-//     grabCursor: 'true',
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//       dynamicBullets: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-
-//     breakpoints:{
-//         0: {
-//             slidesPerView: 1,
-//         },
-//         520: {
-//             slidesPerView: 2,
-//         },
-//         950: {
-//             slidesPerView: 3,
-//         },
-//     },
-//   });
-var swiper = new Swiper(".slide-content", {
+let swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
@@ -49,10 +20,27 @@ var swiper = new Swiper(".slide-content", {
             slidesPerView: 1,
         },
         520: {
-            slidesPerView: 1,
+            slidesPerView: 2,
         },
         950: {
             slidesPerView: 3,
         },
     },
   });
+
+  const items = document.querySelectorAll('.appear2');
+
+  const active = function(entries){
+      entries.forEach(entry => {
+          if(entry.isIntersecting){
+          entry.target.classList.add('inview2'); 
+          }else{
+              entry.target.classList.remove('inview2'); 
+          }
+      });
+  }
+  const io2 = new IntersectionObserver(active);
+   for(let i=0; i < items.length; i++){
+      io2.observe(items[i]);
+   }
+  
